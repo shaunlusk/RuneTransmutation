b2800d329657a16f6a1d62f373063d338fc867b8
Shaun Lusk <slusk@accruent.com>
2017-02-12 22:37:16 -0600

    Level definitions

---
+++ LevelDefinitions.js
@@ -0,0 +1,30 @@
+var levelDefinitions = [
+  {
+    runes : ["fehu", "mannaz", "uruz", "thurisaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE],
+    rows : 5,
+    cols : 5,
+    startingRune : "ingwaz",
+    startingRuneX : 2,
+    startingRuneY : 2
+  }, {
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE],
+    rows : 7,
+    cols : 7,
+    startingRune : "ingwaz",
+    startingRuneX : 3,
+    startingRuneY : 3
+  }, {
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE],
+    rows : 7,
+    cols : 7,
+    startingRune : "ingwaz",
+    startingRuneX : 3,
+    startingRuneY : 3
+  }
+];
