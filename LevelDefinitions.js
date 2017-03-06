17f3481f69fc3540c8671678aa9682bee5fb4f3d
Shaun Lusk <shaun.lusk@gmail.com>
2017-03-06 17:48:29 -0600

    Updated Level definitions

--- LevelDefinitions.js
+++ LevelDefinitions.js
@@ -1,6 +1,6 @@
 var RuneTx = RuneTx || {};
 
-RuneTx.levelDefinitionsVersion = '0.0.2';
+RuneTx.levelDefinitionsVersion = '0.0.3';
 
 RuneTx.levelDefinitions = [
   { // 1
@@ -103,11 +103,186 @@ RuneTx.levelDefinitions = [
   },  { //11
     runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz"],
     runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE],
-    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#2b1400"],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //12
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //13
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //14
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //15
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //16
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //17
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //18
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //19
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //20
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a"],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //21
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //22
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //23
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //24
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //25
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK, C64Style.Color.CYAN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //26
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz", "laguz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK, C64Style.Color.CYAN],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //27
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz", "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz", "laguz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK, C64Style.Color.CYAN, C64Style.Color.BLACK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //28
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz",
+    "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz", "laguz", "dagaz"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK, C64Style.Color.CYAN, C64Style.Color.BLACK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //29
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz",
+    "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz", "laguz", "dagaz", "othala"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK, C64Style.Color.CYAN, C64Style.Color.BLACK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY],
+    rows : 9,
+    cols : 9,
+    startingRune : "ingwaz",
+    startingRuneX : 4,
+    startingRuneY : 4
+  }, { //30
+    runes : ["fehu", "mannaz", "uruz", "thurisaz", "ansuz", "raidho", "kenaz",
+    "wunjo", "nauthiz", "isa", "jera", "eihwas", "perthro", "algiz", "sowilo", "tiwaz", "berkano", "mannaz", "laguz", "dagaz", "othala"],
+    runeColors : [C64Style.Color.YELLOW, C64Style.Color.LIGHTBLUE, C64Style.Color.GREEN,  C64Style.Color.RED, C64Style.Color.PURPLE, C64Style.Color.ORANGE, C64Style.Color.LIGHTGREEN, C64Style.Color.LIGHTGREY, C64Style.Color.PINK, C64Style.Color.CYAN, C64Style.Color.BLACK],
+    boardColors : [C64Style.Color.DARKGREY, C64Style.Color.BLUE, "#4c3d2a", C64Style.Color.GREY, C64Style.Color.BLACK],
     rows : 9,
     cols : 9,
     startingRune : "ingwaz",
     startingRuneX : 4,
     startingRuneY : 4
   }
+
 ];
